f9463e2f9949187a81b7cd7117998b884c1c4b13
franciscoLopes <franciscolopesr26@gmail.com~>
2021-01-13 09:20:39 +0000

    facilmente crud

---
+++ MVC/MVC/Models/contexto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+namespace MVC.Models
+{
+    public class contexto : DbContext
+    {
+         public DbSet<Pessoa> pessoas { get; set; }
+        
+
+    }
+}
